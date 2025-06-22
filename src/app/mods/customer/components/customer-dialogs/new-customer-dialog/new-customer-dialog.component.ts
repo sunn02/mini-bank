d985d7048e231cfc56ab40152fb5616ceca6c3ce
sunn02 <sollopez0002@hotmail.com>
2025-06-22 01:09:17 -0300

    refactor: new-costumer interface

--- src/app/mods/customer/components/customer-dialogs/new-customer-dialog/new-customer-dialog.component.ts
+++ src/app/mods/customer/components/customer-dialogs/new-customer-dialog/new-customer-dialog.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SHARED_PRIMENG_MODULES } from '../../../../../shared/shared-primeng';
 import { Customer } from '../../../models/customer.model';
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { CustomerApiService } from '../../../services/customer-api.service';
+import { Bank } from '../../../../banks/models/bank.model';
+import { CommonModule } from '@angular/common';
+import { BankApiService } from '../../../../banks/services/bank-api.service';
 
 
 @Component({
-    imports: [FormsModule, SHARED_PRIMENG_MODULES],
+    imports: [FormsModule, SHARED_PRIMENG_MODULES, CommonModule],
     selector: 'app-new-Customer-dialog',
     templateUrl: './new-customer-dialog.component.html',
     styleUrl: './new-customer-dialog.component.css',
 })
-export class NewCustomerDialogComponent{
+export class NewCustomerDialogComponent implements OnInit{
 
     model: Customer = {
         name: "",
@@ -26,12 +30,28 @@ export class NewCustomerDialogComponent{
     }
 
 
-    constructor (public ref: DynamicDialogRef) {};
+    banks: Bank[] = []; 
 
-    onAddDialog(){
-        this.ref.close(this.model);
+
+    constructor (public ref: DynamicDialogRef, private apiService: CustomerApiService, private bankApi: BankApiService) {};
+
+    save(){
+        this.apiService.postData(this.model).subscribe({
+            next: (response) => this.ref.close(response), 
+            error: (err) => console.error('Error:', err)
+        })
     }
 
 
+    getBanks(){
+        this.bankApi.getData().subscribe({ 
+        next: data => { this.banks = <Bank[]>data} 
+        }
+    )
+    }
+    
+    ngOnInit() {
+        this.getBanks(); 
+    }
 
 }
