cfb9722da4a344fcbf369bd7370ed4996d9c921e
sunn02 <sollopez0002@hotmail.com>
2025-06-25 14:52:31 -0300

    chore: delete customer-data.service.ts and customer.service.ts

--- src/app/mods/customer/services/customer-data.service.ts
+++
@@ -1,6 +0,0 @@
-// import { Customer } from "../models/customer.model";
-
-// export const Customers_DB: Customer[] = [
-//     { id: 1, name: 'John Doe', document: '1234' },
-//     { id: 2, name: 'Jane Smith', document: '5678' },
-//   ];
