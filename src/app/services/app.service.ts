d8801412862ca65829215ac7ed8ab40b23268b11
sunn02 <sollopez0002@hotmail.com>
2025-06-27 17:29:32 -0300

    chore: clean up code

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Customer } from '../mods/customer/models/customer.model';
-import { Bank } from '../mods/banks/models/bank.model';
-import { Account } from '../mods/accounts/models/account.model';
 import { CustomerApiService } from '../mods/customer/services/customer-api.service';
 import { BankApiService } from '../mods/banks/services/bank-api.service';
 import { AccountApiService } from '../mods/accounts/services/account-api.service';
 import { CurrencyApiService } from '../mods/currency/services/currency-api.service';
-import { Currency } from '../mods/currency/models/currency.model';
 
 @Injectable({
   providedIn: 'root'
